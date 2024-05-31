3cef8bffd1f352d150009901474c26584fd80995
hearofnight <63850765+hearofnight@users.noreply.github.com>
2024-05-31 17:49:38 +0800

    Update http.go

--- utils/http.go
+++ utils/http.go
@@ -16,12 +16,9 @@ func GetTestVideoUrl(w http.ResponseWriter) {
 	}
 	str_time := time.Now().In(TimeLocation).Format("2006-01-02 15:04:05")
   fmt.Fprintln(w, "#EXTM3U")
-  fmt.Fprintln(w, "#EXTINF:-1 tvg-name=\""+str_time+"\" tvg-logo=\"https://cdn.jsdelivr.net/gh/youshandefeiyang/IPTV/logo/tg.jpg\" group-title=\"列表更新时间\","+str_time)
-  fmt.Fprintln(w, "https://cdn.jsdelivr.net/gh/youshandefeiyang/testvideo/time/time.mp4")
-  fmt.Fprintln(w, "#EXTINF:-1 tvg-name=\"4K60PSDR-H264-AAC测试\" tvg-logo=\"https://cdn.jsdelivr.net/gh/youshandefeiyang/IPTV/logo/tg.jpg\" group-title=\"4K频道\",4K60PSDR-H264-AAC测试")
-  fmt.Fprintln(w, "http://159.75.85.63:5680/d/ad/h264/playad.m3u8")
-  fmt.Fprintln(w, "#EXTINF:-1 tvg-name=\"4K60PHLG-HEVC-EAC3测试\" tvg-logo=\"https://cdn.jsdelivr.net/gh/youshandefeiyang/IPTV/logo/tg.jpg\" group-title=\"4K频道\",4K60PHLG-HEVC-EAC3测试")
-  fmt.Fprintln(w, "http://159.75.85.63:5680/d/ad/playad.m3u8")
+  fmt.Fprintln(w, "#EXTINF:-1 tvg-name=\""+str_time+"\" tvg-logo=\"http://epg.51zmt.top:8000/tb1/CCTV/CCTV1.png\" group-title=\"列表更新时间\","+str_time)
+  fmt.Fprintln(w, "http://live.hearofnight.cn:5244/d/alicloud/IPTV/StratMedia/%E5%B8%88%E5%A7%90%E4%B8%80%E5%8F%8C%E8%84%9A%EF%BC%8C%E4%BD%A0%E6%9C%89%E4%B8%80%E5%BC%A0%E5%98%B4.mp4")
+}
 }
 
 func GetLivePrefix(r *http.Request) string {
